c217604d4b2ef13e3f81c22ee8a8903405b6e041
Iupiter1007 <45148618+Iupiter1007@users.noreply.github.com>
2025-12-05 00:03:21 -0700

    remove test file
    remove test file

---
+++ input.py
@@ -0,0 +1,10 @@
+# marzuk hasan
+## Inputs for Mojave Sphinx Analysis
+
+# Ambient Conditions
+ambient = {
+"p_kPa": = 101325,  # Ambient pressure in Pa
+"T_C" = 20,  # Ambient temperature in C
+"T_K" = T_amb_C + 273.15  # Convert to Kelvin
+
+}
